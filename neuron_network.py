6b1ee62693bd72083a77236f71d89e7bc06fd10a
Raul Rayo <rayo.in@gmail.com>
2018-11-06 00:30:22 -0300

    falta ver si el error en las hidden layers se calcula así

--- neuron_network.py
+++ neuron_network.py
@@ -1,6 +1,4 @@
 # TODO unittest training (?) -> ver si se usa
-# TODO clases network, layers
-# TODO forwardfeed, backwardfeed, error propagation, weightrecalculation
 
 import numpy as np
 
@@ -37,13 +35,12 @@ class NewronNetwork:
                     self.layers[i].neurons[k].adjust_delta(error)
                 last = False
             else:
-                # if i == 0:
-                #     break
-
                 # por cada neurona del layer se calcula el error y actualiza su delta
                 for k in range(len(self.layers[i].neurons)):  # TODO refactor (meter en layers y en neurona)
                     error = 0
-                    for neuron in self.layers[i + 1].neurons:
+                    for neuron in self.layers[i + 1].neurons: # TODO revisar si está bien
+                        # la neurona 1 del primer layer tendría un error:
+                        # n1_layer2_w1*n1_layer2_delta + n2_layer2_w2*n2_layer2_delta + ...
                         error += neuron.weights[k] * neuron.delta
                     self.layers[i].neurons[k].adjust_delta(error)
 
