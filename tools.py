8de48b336b306d9b95a26c96fb05c63edb21c502
Raul Rayo <rayo.in@gmail.com>
2018-11-07 18:21:36 -0300

    ahora later_const crea una red neuronal se agrega normalización y desnormaliación de data

--- tools.py
+++ tools.py
@@ -1,10 +1,13 @@
+import numpy as np
+
 from neuron import Neuron
 from neuron_layer import NeuronLayer
+from neuron_network import NeuronNetwork
 
 
-def layer_const(shape, fun):
+def net_constructor(shape, fun):
     """
-    Crea un arreglo de layers de neuronas a partir de un shape.
+    Crea una red neuronal a partir de un shape.
     :param shape: tupla o lista de la forma: (inputs, num_neuronas_layer_1, layer_2, ...).
 
     :return: arreglo con layers de neuronas.
@@ -26,4 +29,18 @@ def layer_const(shape, fun):
         layers.append(NeuronLayer(neurons))
 
         aux = val
-    return layers
+    return NeuronNetwork(layers)
+
+
+def normalize_data_fun(data_low, data_high, n_low, n_high):
+    return lambda x: (x - data_low) * (n_high - n_low) / (data_high - data_low) + n_low
+
+
+def denormalize_data_fun(data_low, data_high, n_low, n_high):
+    return lambda x: ((data_low - data_high) * x - (n_high * data_low) + (data_high * n_low)) / (n_low - n_high)
+
+
+# f = normalize_data_fun(-10, 10, 0, 1)
+# f2 = denormalize_data_fun(-10, 10, 0, 1)
+# print(f(5))
+# print(f2(0.75))
