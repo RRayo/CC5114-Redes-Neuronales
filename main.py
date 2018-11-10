977e9f01246d3bceb5e631f53fe81b79b09560d9
Raul Rayo <rayo.in@gmail.com>
2018-11-09 21:14:25 -0300

    main final

--- main.py
+++ main.py
@@ -14,15 +14,18 @@ def main():
 
     learning_rates = [0.1, 0.3, 0.5, 0.8, 1, 3]
 
-    # for shape in net_shapes:
-    #     process_iris(activation_fun=activation_function, net_shape=shape, plot=False)
+    print("DISTINTAS REDES")
+    for shape in net_shapes:
+        process_iris(activation_fun=activation_function, net_shape=shape, plot=False)
 
+    print("DISTINTOS LEARNING RATES")
     for lr in learning_rates:
         process_iris(activation_fun=activation_function, learning_rate=lr, plot=False)
 
-    # for i in range(5):
-    #     random_state = np.random.randint(1, 100)
-    #     process_iris(title=f"red_basica_{i}_{random_state}", random_state=random_state, net_shape=(4, 4, 3))
+    print("DISTINTOS INPUTS")
+    for i in range(5):
+        random_state = np.random.randint(1, 100)
+        process_iris(title=f"red_basica_{i}_{random_state}", random_state=random_state)
 
 
 if __name__ == '__main__':
