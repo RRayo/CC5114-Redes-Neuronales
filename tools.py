98091aa91a44a6281f5c3f847013d12458c70c18
Raul Rayo <rayo.in@gmail.com>
2018-12-01 18:36:38 -0300

    MOD: función generalizada para graficar

--- tools.py
+++ tools.py
@@ -1,5 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+import re
+
+FOLDER_GRAPHS = os.path.join(os.path.dirname(__file__), "results", "")
 
 
 # ordenar poblacion por el fitness del arreglo
@@ -10,14 +14,25 @@ def sortPopulation(population, fitness_array):
     return population[inds].tolist()
 
 
-def graficar(rango, data, problema, xlabel, ylabel, mutation_rate=-1, pop_size=-1, plot=False):
-    plt.plot(rango, data)
-    plt.title(f"Problema {problema}, mutation_rate={mutation_rate} ,pop_size={pop_size}")
+def graficar(rango, data, problema, xlabel, ylabel, tipo=1, mutation_rate=-1, pop_size=-1, ticks=False, plot=False):
+    if tipo == 1:
+        plt.plot(rango, data)
+    elif tipo == 2:
+        plt.scatter(rango, data)
+    else:
+        plt.bar(rango, data)
+        if ticks:
+            plt.xticks(rango, ticks)
+
+    if mutation_rate>0 and pop_size>0:
+        plt.title(f"Problema {problema}, mutation_rate={mutation_rate} ,pop_size={pop_size}")
+    else:
+        plt.title(f"{problema}")
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     file_name = problema + str(mutation_rate).replace(".", "") + str(pop_size) + ".jpg"
+    file_name = re.sub("[\s_]+", "", file_name)
     plt.savefig(FOLDER_GRAPHS + file_name)
     if plot:
         plt.show()
     plt.close()
-
