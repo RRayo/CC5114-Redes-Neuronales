1def1d38f1e6101409e48b9c2f76f3b202f36750
Raul Rayo <rayo.in@gmail.com>
2018-12-01 17:04:39 -0300

    ADD: función graficar

--- tools.py
+++ tools.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 # ordenar poblacion por el fitness del arreglo
@@ -9,6 +10,14 @@ def sortPopulation(population, fitness_array):
     return population[inds].tolist()
 
 
-def graficar(rango, data, problema, mutation_rate=-1, pop_size=-1):
-
+def graficar(rango, data, problema, xlabel, ylabel, mutation_rate=-1, pop_size=-1, plot=False):
+    plt.plot(rango, data)
+    plt.title(f"Problema {problema}, mutation_rate={mutation_rate} ,pop_size={pop_size}")
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    file_name = problema + str(mutation_rate).replace(".", "") + str(pop_size) + ".jpg"
+    plt.savefig(FOLDER_GRAPHS + file_name)
+    if plot:
+        plt.show()
+    plt.close()
 
