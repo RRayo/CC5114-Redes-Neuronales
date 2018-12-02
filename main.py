0caea388cb7feb62d9c58b1020368c6f48b733c5
Raul Rayo <rayo.in@gmail.com>
2018-12-01 21:37:44 -0300

    MOD: Nueva forma de obtener resultados generales

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from fitness.bits_fitness import BitFitness
 from fitness.nqueens_fit import NQueensFit
 from genetic_algorithm import GeneticAlgorithm
@@ -5,7 +7,6 @@ from population.pop_nqueens import PopNQueens
 from population.population import Population
 from reproduction.reproduction import Reproduction
 from selection.selection import Selection
-
 from tools import graficar
 
 
@@ -57,59 +58,82 @@ def main():  # TODO hacer clase/función que reciba los parámetros mutacion_rat
     pop_array = [len_genes * x for x in [2, 5, 10, 20]]
     mutation_array = [0.05, 0.1, 0.3, 0.5]
 
-    time_pop = []
-    time_mutation = []
-    generations_pop = []
-    generations_mutation = []
+    time_pop_global = []
+    time_mutation_global = []
+    generations_pop_global = []
+    generations_mutation_global = []
 
-    for i in range(len(pop_array)):
-        pop_size = pop_array[i]
-        print("Pop size: ", pop_size)
+    n_exp = 10
 
-        mutation_rate = 0.1
-        max_fit = 1.0  # minimo numero de errores
-        ga_string = GeneticAlgorithm(len_genes, genes, pop_size, mutation_rate, max_fit)
-        fitness_max_history, fitness_mean_history, counter, time = ga_string.run(PopNQueens, NQueensFit, Selection,
-                                                                                 Reproduction, ref_individual=False)
+    for x in range(n_exp):
 
-        graficar(range(len(fitness_max_history)), fitness_max_history, f"N-Queens {len_genes} (max_fit)",
-                 "Generaciones", "Max fitness", mutation_rate=mutation_rate, pop_size=pop_size, tipo=3)
-        graficar(range(len(fitness_mean_history)), fitness_mean_history, f"N-Queens {len_genes} (mean_fit)",
-                 "Generaciones", "Mean fitness", mutation_rate=mutation_rate, pop_size=pop_size, tipo=3)
-        print("Generaciones: ", counter)
-        print("Tiempo: ", time)
-        print()
-        time_pop.append(time)
-        generations_pop.append(counter)
+        time_pop = []
+        time_mutation = []
+        generations_pop = []
+        generations_mutation = []
 
-    for i in range(len(mutation_array)):
-        mutation_rate = mutation_array[i]
-        print("mutation_rate: ", mutation_rate)
+        print("Experimento", x)
 
-        pop_size = len_genes * 10
-        max_fit = 1.0  # minimo numero de errores
-        ga_string = GeneticAlgorithm(len_genes, genes, pop_size, mutation_rate, max_fit)
-        fitness_max_history, fitness_mean_history, counter, time = ga_string.run(PopNQueens, NQueensFit, Selection,
-                                                                                 Reproduction, ref_individual=False)
+        for i in range(len(pop_array)):
+            pop_size = pop_array[i]
+            # print("Pop size: ", pop_size)
 
-        graficar(range(len(fitness_max_history)), fitness_max_history, f"N-Queens {len_genes} (max_fit)",
-                 "Generaciones", "Max fitness", mutation_rate=mutation_rate, pop_size=pop_size, tipo=3)
-        graficar(range(len(fitness_mean_history)), fitness_mean_history, f"N-Queens {len_genes} (mean_fit)",
-                 "Generaciones", "Mean fitness", mutation_rate=mutation_rate, pop_size=pop_size, tipo=3)
-        print("Generaciones: ", counter)
-        print("Tiempo: ", time)
-        print()
-        time_mutation.append(time)
-        generations_mutation.append(counter)
+            mutation_rate = 0.1
+            max_fit = 1.0  # minimo numero de errores
+            ga_string = GeneticAlgorithm(len_genes, genes, pop_size, mutation_rate, max_fit)
+            fitness_max_history, fitness_mean_history, counter, time = ga_string.run(PopNQueens, NQueensFit, Selection,
+                                                                                     Reproduction, ref_individual=False)
 
-    graficar(range(len(time_pop)), time_pop, "Tiempo poblaciones", xlabel="Poblaciones", ylabel="Tiempo", tipo=3,
+            # graficar(range(len(fitness_max_history)), fitness_max_history, f"N-Queens {len_genes} (max_fit)",
+            #          "Generaciones", "Max fitness", mutation_rate=mutation_rate, pop_size=pop_size, tipo=3)
+            # graficar(range(len(fitness_mean_history)), fitness_mean_history, f"N-Queens {len_genes} (mean_fit)",
+            #          "Generaciones", "Mean fitness", mutation_rate=mutation_rate, pop_size=pop_size, tipo=3)
+            # print("Generaciones: ", counter)
+            # print("Tiempo: ", time)
+            # print()
+            time_pop.append(time)
+            generations_pop.append(counter)
+
+        for i in range(len(mutation_array)):
+            mutation_rate = mutation_array[i]
+            # print("mutation_rate: ", mutation_rate)
+
+            pop_size = len_genes * 10
+            max_fit = 1.0  # minimo numero de errores
+            ga_string = GeneticAlgorithm(len_genes, genes, pop_size, mutation_rate, max_fit)
+            fitness_max_history, fitness_mean_history, counter, time = ga_string.run(PopNQueens, NQueensFit, Selection,
+                                                                                     Reproduction, ref_individual=False)
+
+            # graficar(range(len(fitness_max_history)), fitness_max_history, f"N-Queens {len_genes} (max_fit)",
+            #          "Generaciones", "Max fitness", mutation_rate=mutation_rate, pop_size=pop_size, tipo=3)
+            # graficar(range(len(fitness_mean_history)), fitness_mean_history, f"N-Queens {len_genes} (mean_fit)",
+            #          "Generaciones", "Mean fitness", mutation_rate=mutation_rate, pop_size=pop_size, tipo=3)
+            # print("Generaciones: ", counter)
+            # print("Tiempo: ", time)
+            # print()
+            time_mutation.append(time)
+            generations_mutation.append(counter)
+
+        time_pop_global.append(time_pop)
+        time_mutation_global.append(time_mutation)
+        generations_pop_global.append(generations_pop)
+        generations_mutation_global.append(generations_mutation)
+
+    time_pop_global = np.sum(np.array(time_pop_global), axis=0)/n_exp
+    time_mutation_global = np.sum(np.array(time_mutation_global), axis=0)/n_exp
+    generations_pop_global = np.sum(np.array(generations_pop_global), axis=0)/n_exp
+    generations_mutation_global = np.sum(np.array(generations_mutation_global), axis=0)/n_exp
+
+    graficar(range(len(time_pop_global)), time_pop_global, "Tiempo poblaciones", xlabel="Poblaciones", ylabel="Tiempo",
+             tipo=3,
              ticks=pop_array)
-    graficar(range(len(time_mutation)), time_mutation, "Tiempo mutations_rates", xlabel="Mutation rates",
+    graficar(range(len(time_mutation_global)), time_mutation_global, "Tiempo mutations_rates", xlabel="Mutation rates",
              ylabel="Tiempo", tipo=3, ticks=mutation_array)
 
-    graficar(range(len(generations_pop)), generations_pop, "Generaciones poblaciones", xlabel="Poblaciones",
+    graficar(range(len(generations_pop_global)), generations_pop_global, "Generaciones poblaciones",
+             xlabel="Poblaciones",
              ylabel="Generaciones", tipo=3, ticks=pop_array)
-    graficar(range(len(generations_mutation)), generations_mutation, "Generaciones mutations_rates",
+    graficar(range(len(generations_mutation_global)), generations_mutation_global, "Generaciones mutations_rates",
              xlabel="Mutation rates", ylabel="Generaciones", tipo=3, ticks=mutation_array)
 
 
